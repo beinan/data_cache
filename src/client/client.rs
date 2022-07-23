dbc094e4f78f518a9eeb5ccc1ddbc499ae794702
Beinan Wang <soros.wang@yahoo.com>
2022-07-22 23:38:16 -0700

    Link grpc package to client

--- src/client/client.rs
+++ src/client/client.rs
@@ -17,34 +17,7 @@ use settings::Settings;
 
 mod settings;
 
-pub mod alluxio {
-    pub mod grpc {
-        tonic::include_proto!("alluxio.grpc");
-        pub mod block {
-            tonic::include_proto!("alluxio.grpc.block");
-        }
-        pub mod sasl {
-            tonic::include_proto!("alluxio.grpc.sasl");
-        }
-        pub mod file {
-            tonic::include_proto!("alluxio.grpc.file");
-        }
-        pub mod fscommon {
-            tonic::include_proto!("alluxio.grpc.fscommon");
-        }
-    }
-    pub mod proto {
-        pub mod dataserver {
-            tonic::include_proto!("alluxio.proto.dataserver");
-        }
-        pub mod journal {
-            tonic::include_proto!("alluxio.proto.journal");
-        }
-        pub mod shared {
-            tonic::include_proto!("alluxio.proto.shared");
-        }
-    }
-}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -106,8 +79,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
     }
-
-
 
     Ok(())
 }
