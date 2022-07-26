e11ca88b38d14dc17689278ee90a8850a8408301
Beinan Wang <soros.wang@yahoo.com>
2022-07-26 11:51:08 -0700

    Refine the output of list status

--- client/src/cmds/ls.rs
+++ client/src/cmds/ls.rs
@@ -1,5 +1,9 @@
+use std::fs::File;
 use structopt::StructOpt;
 use alluxio_grpc::grpc_client::Client;
+use alluxio_grpc::grpc::file::FileInfo;
+
+use tabled::{Tabled, Table};
 
 #[derive(StructOpt, Debug)]
 pub struct ListOptions {
@@ -8,13 +12,28 @@ pub struct ListOptions {
     path: String,
 }
 
+#[derive(Tabled)]
+struct FileRow {
+    id: i64,
+    name: String,
+    ufs_path: String,
+    length: i64,
+}
+
 pub async fn ls(client : Client, options: &ListOptions) -> Result<String, String> {
     println!("List Status start");
     match client.master_client().unwrap().listStatus(String::from(options.path.clone())).await {
         Ok(fileInfos) => {
-            for file in fileInfos {
-                println!("{:?}", file);
-            }
+            // for file in fileInfos {
+            //     println!("{:?}", file);
+            // }
+            let rows: Vec<FileRow> = fileInfos.into_iter().map(|fileInfo|FileRow {
+                id: fileInfo.file_id.unwrap(),
+                name: fileInfo.name.unwrap(),
+                ufs_path: fileInfo.ufs_path.unwrap(),
+                length: fileInfo.length.unwrap(),
+            }).collect();
+            println!("{}", Table::new(rows).to_string());
             Ok(String::from("ok"))
         },
         Err(err) => {
