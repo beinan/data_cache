80df64b408fe4a19c8d2eabb9be8950dc4a9ea9c
Beinan <beinan@alluxio.com>
2022-09-20 15:53:43 -0700

    Implement get and put of the cache manager

--- cache_jni/java_lib/src/test/java/alluxio/sdk/file/cache/NativeCacheManagerTest.java
+++ cache_jni/java_lib/src/test/java/alluxio/sdk/file/cache/NativeCacheManagerTest.java
@@ -1,8 +1,12 @@
 package alluxio.sdk.file.cache;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Unit test for simple App.
@@ -16,6 +20,11 @@ public class NativeCacheManagerTest
     public void shouldAnswerWithTrue()
     {
         NativeCacheManager manager = new NativeCacheManager();
-        assertTrue(manager.put(new byte[5], new byte[5]));
+        assertTrue(manager.init(128 * 1024 * 1024, 1024 * 1024, 16));
+        assertTrue(manager.put("key1".getBytes(), "test_value".getBytes()));
+        byte[] result = new byte[5];
+        int bytesread = manager.get("key1".getBytes(), 0, 5, result, 0);
+        assertEquals(5, bytesread);
+        assertEquals("test_", new String(result, StandardCharsets.UTF_8));
     }
 }
--- cache_jni/rust_lib/src/lib.rs
+++ cache_jni/rust_lib/src/lib.rs
@@ -1,21 +1,65 @@
+use std::ops::{Deref, DerefMut};
+use std::sync::MutexGuard;
+use std::time::Duration;
+
 use jni::JNIEnv;
-use jni::sys::{jboolean, jbyteArray, jint, JNI_TRUE, jlong};
+use jni::sys::{jboolean, jbyteArray, jint, JNI_TRUE, jlong, JNI_FALSE};
 use jni::objects::JObject;
+
+use segcache::{Policy, Seg, Value};
 
 #[no_mangle]
 pub extern "system" fn Java_alluxio_sdk_file_cache_NativeCacheManager_get(
-    _env: JNIEnv, _obj: JObject, key: jbyteArray, page_offset: jint,  bytes_to_read: jint, buffer: jbyteArray, offset_in_buf: jint) -> jint {
-    5
+    env: JNIEnv, obj: JObject, key: jbyteArray, page_offset: jint,  bytes_to_read: jint, buffer: jbyteArray, offset_in_buf: jint) -> jint {
+        let cache = env.get_rust_field::<_, _, Seg>(obj, "nativeCacheHandle");
+        match cache {
+            Ok(mut guard) => {
+                let seg = guard.deref_mut();
+                let rust_key = &env.convert_byte_array(key).unwrap()[..];
+                let item = seg.get(rust_key);
+                if item.is_none() {
+                    -1
+                } else {
+                    match item.unwrap().value() {
+                        Value::Bytes(v) => {
+                            let mut slice = &v[page_offset as usize .. (page_offset + bytes_to_read) as usize];
+                            let slice = unsafe{ &*(slice as *const [u8] as *const [i8])};
+                            env.set_byte_array_region(buffer, offset_in_buf, slice).unwrap();
+                            bytes_to_read as i32
+                        },
+                        Value::U64(l) => core::mem::size_of::<u64>() as i32,       
+                    }
+                }
+            },
+            Err(_) => -1   
+        }    
 }
 
 #[no_mangle]
 pub extern "system" fn Java_alluxio_sdk_file_cache_NativeCacheManager_put(env: JNIEnv, obj: JObject, key: jbyteArray, page: jbyteArray) -> jboolean {
-    return JNI_TRUE
+    let cache = env.get_rust_field::<_, _, Seg>(obj, "nativeCacheHandle");
+    match cache {
+        Ok(mut guard) => {
+            let seg = guard.deref_mut();
+            let rust_key = &env.convert_byte_array(key).unwrap()[..];
+            let rust_value = &env.convert_byte_array(page).unwrap();
+            seg.insert(rust_key, rust_value, None, Duration::ZERO);
+            JNI_TRUE
+        },
+        Err(_) => JNI_FALSE
+    }
 }
 
 #[no_mangle]
 pub extern "system" fn Java_alluxio_sdk_file_cache_NativeCacheManager_init(env: JNIEnv, obj: JObject, cache_size: jlong, seg_size: jint, hash_power: jint) -> jboolean {
-    
-    return JNI_TRUE
+    let cache = Seg::builder()
+        .heap_size(cache_size as usize)
+        .segment_size(seg_size)
+        .hash_power(hash_power as u8)
+        .eviction(Policy::Random).build().expect("failed to create cache");
+    match env.set_rust_field(obj, "nativeCacheHandle", cache) {
+        Ok(_) => JNI_TRUE,
+        Err(_) => JNI_FALSE,
+    }
 }
 
